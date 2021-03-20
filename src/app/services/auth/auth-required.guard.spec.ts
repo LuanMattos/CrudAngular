750df923bb5c0998b5fe8834966a44261a7d7031
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-03-20 16:23:05 -0300

    Testes Auth Require Guard

--- src/app/services/auth/auth-required.guard.spec.ts
+++ src/app/services/auth/auth-required.guard.spec.ts
@@ -1,7 +1,9 @@
 import {inject, TestBed} from '@angular/core/testing';
+import {AuthRequiredGuard} from './auth-required.guard';
 import {RouterTestingModule} from '@angular/router/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {AuthRequiredGuard} from './auth-required.guard';
+import {DragoesService} from '../dragoes.service';
+
 
 describe('AuthRequiredGuard', () => {
 
@@ -11,8 +13,13 @@ describe('AuthRequiredGuard', () => {
       HttpClientTestingModule
     ],
     providers: [
+      DragoesService,
       AuthRequiredGuard
     ]
+  }));
+
+  it('Validação authGuard', inject([AuthRequiredGuard], (service: AuthRequiredGuard) => {
+    expect(service.canActivate()).toBeFalse();
   }));
 
 });
