897fb0f894f913c33f568f738baff974898b5ef9
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-03-21 12:51:29 -0300

    favicon e ajuste notfound component

--- src/app/not-found/not-found.component.spec.ts
+++ src/app/not-found/not-found.component.spec.ts
@@ -1,12 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { NotFoundComponent } from './not-found.component';
 
 describe('NotFoundComponent', () => {
+  let component: NotFoundComponent;
+  let fixture: ComponentFixture<NotFoundComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         NotFoundComponent
       ],
     }).compileComponents();
+  });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NotFoundComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 });
